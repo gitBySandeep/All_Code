8a9f70d867eafe276bac68ca44a64281335c0db9
rajsinghthakure <rajthakur8827142011@gmail.com>
2024-04-05 14:52:56 +0530

    Added ayurveda folder

---
+++ ayurveda/src/components/Footer/Footer.js
@@ -0,0 +1,22 @@
+import "./Footer.css";
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer">
+                <div className="footer">
+                    <div className="footer"></div>
+                    <div className="footer"></div>
+                    <div className="footer"></div>
+                    <div className="footer">
+                        <div className="footer"></div>
+                        <div className="footer"></div>
+                    </div>
+                </div>
+                <div className="footer"></div>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
