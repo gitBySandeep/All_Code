83a5c90b8a984bd5bc5a5254f9d8950690165c62
rajsinghthakure <rajthakur8827142011@gmail.com>
2024-04-12 12:06:55 +0530

    delete old header and add new responsive header and modify home & user folder

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,51 +2,53 @@ import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div className="footer border" style={{ backgroundColor: "var(--green)", color: "var(--white)", width: "99vw" }}>
-            <div className="footer-inner m-1 ms-3 me-3">
-                <div className="footer-top d-flex justify-content-between flex-wrap">
-                    <div className="footer-customersupport  d-flex flex-column p-3">
-                        <h3 className="footer-h3">Customer Support</h3>
-                        <small className="footer-values links ps-3">Contact Us</small>
-                        <small className="footer-values links ps-3">FAQ</small>
-                        <small className="footer-values links ps-3">Track Order</small>
-                        <small className="footer-values links ps-3">Return & Exchange</small>
-                        <small className="footer-values links ps-3">Shipping Location</small>
-                    </div>
-                    <div className="footer-aboutus d-flex flex-column p-3">
-                        <h3 className="footer-h3">About Us</h3>
-                        <small className="footer-values links ps-3">Our Story</small>
-                        <small className="footer-values links ps-3">Blogs</small>
-                        <small className="footer-values links ps-3">Ingredients Index</small>
-                        <small className="footer-values links ps-3">Hotel & Spa</small>
-                        <small className="footer-values links ps-3">Media</small>
-                    </div>
-                    <div className="footer-account  d-flex flex-column p-3">
-                        <h3 className="footer-h3">Your Account</h3>
-                        <small className="footer-values links ps-3">My Account</small>
-                    </div>
-                    <div className="footer-privacypolicy ">
-                        <div className="footer-privacypolicy-top d-flex flex-column p-3">
-                            <h3 className="footer-h3">Privacy and Terms</h3>
-                            <small className="footer-values links ps-3">Privacy Policy</small>
-                            <small className="footer-values links ps-3">Terms & Conditions</small>
+        <>
+            <div className="footer border" style={{ backgroundColor: "var(--green)", color: "var(--white)", width: "99vw" }}>
+                <div className="footer-inner m-1 ms-3 me-3">
+                    <div className="footer-top d-flex justify-content-between flex-wrap">
+                        <div className="footer-customersupport  d-flex flex-column p-3">
+                            <h3 className="footer-h3">Customer Support</h3>
+                            <small className="footer-values links ps-3">Contact Us</small>
+                            <small className="footer-values links ps-3">FAQ</small>
+                            <small className="footer-values links ps-3">Track Order</small>
+                            <small className="footer-values links ps-3">Return & Exchange</small>
+                            <small className="footer-values links ps-3">Shipping Location</small>
                         </div>
-                        <div className="footer-privacypolicy d-flex justify-content-evenly flex-wrap social-icons">
-                            <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-google"></div>
-                            <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-facebook"></div>
-                            <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-git"></div>
-                            <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-linkedin"></div>
+                        <div className="footer-aboutus d-flex flex-column p-3">
+                            <h3 className="footer-h3">About Us</h3>
+                            <small className="footer-values links ps-3">Our Story</small>
+                            <small className="footer-values links ps-3">Blogs</small>
+                            <small className="footer-values links ps-3">Ingredients Index</small>
+                            <small className="footer-values links ps-3">Hotel & Spa</small>
+                            <small className="footer-values links ps-3">Media</small>
+                        </div>
+                        <div className="footer-account  d-flex flex-column p-3">
+                            <h3 className="footer-h3">Your Account</h3>
+                            <small className="footer-values links ps-3">My Account</small>
+                        </div>
+                        <div className="footer-privacypolicy">
+                            <div className="footer-privacypolicy-top d-flex flex-column p-3">
+                                <h3 className="footer-h3">Privacy and Terms</h3>
+                                <small className="footer-values links ps-3">Privacy Policy</small>
+                                <small className="footer-values links ps-3">Terms & Conditions</small>
+                            </div>
+                            <div className="footer-privacypolicy d-flex justify-content-evenly flex-wrap social-icons">
+                                <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-google"></div>
+                                <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-facebook"></div>
+                                <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-git"></div>
+                                <div style={{ filter: "invert(98%) sepia(57%) saturate(10%) hue-rotate(227deg) brightness(104%) contrast(102%)" }} className="border-dark footer-icon icon-linkedin"></div>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="footer-bottom d-flex align-items-center flex-wrap">
-                    <div className="footer-logo ms-3"></div>
-                    <div className="footer-text d-flex flex-wrap ms-3">
-                        <h6>Worldwide Copyright © The Great Ayurveda  (brand owners of The Great Ayurveda). All rights reserved.</h6>
+                    <div className="footer-bottom d-flex align-items-center flex-wrap">
+                        <div className="footer-logo ms-3"></div>
+                        <div className="footer-text d-flex flex-wrap ms-3">
+                            <h6>Worldwide Copyright © The Great Ayurveda  (brand owners of The Great Ayurveda). All rights reserved.</h6>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- frontend/src/components/Homeremedy/HomeremedyDescription.js
+++ frontend/src/components/Homeremedy/HomeremedyDescription.js
@@ -16,27 +16,29 @@ export default function ViewMore() {
 
     return <>
         <Header />
-        <div className="mt-3 mb-4">
-            <div className="d-flex align-items-start justify-content-evenly m-5 mt-2 p-3 flex-wrap">
-                <div className="d-flex flex-column" style={{ height: "400px", width: "600px" }}>
-                    <img src={state.imageUrl} style={{ height: "100%", width: '100%' }} />
-                    <span className="text-center fs-2 mt-3 mb-3" >{state.remedyName}</span>
+        <div className="rmm mt-3 mb-4 ">
+            <div className=" rmm d-flex align-items-start justify-content-evenly m-5 mt-2 p-3 flex-wrap ">
+                <div className="rmm d-flex flex-column ">
+                    <div className="rmm remedyimg" style={{ height: "400px", width: "600px" }}>
+                        <img src={state.imageUrl} style={{ height: "100%", width: '100%' }} />
+                    </div>
+                    <span className="rmm text-center fs-2 mt-3 mb-3 " >{state.remedyName}</span>
                 </div>
-                <div className="ms-4 d-flex flex-column" style={{ width: "600px" }}>
-                    <span className="fs-4">Homeremedy {state.remedyName}</span>
-                    <span className="mt-2 fs-6">{state.description}</span>
-                    <span className="mt-2 fs-4"> Ingredients of {state.remedyName}</span>
-                    <span className="mt-2">{state.ingredients}</span>
-                    <span className="mt-2 fs-4"> Instructions of {state.remedyName}</span>
+                <div className="rmm remedydata ms-4 d-flex flex-column " style={{ width: "600px" }}>
+                    <span className="rmm fs-4">Homeremedy {state.remedyName}</span>
+                    <span className="rmm mt-2 fs-6">{state.description}</span>
+                    <span className="rmm mt-2 fs-4"> Ingredients of {state.remedyName}</span>
+                    <span className="rmm mt-2 fs-6">{state.ingredients}</span>
+                    <span className="rmm mt-2 fs-4"> Instructions of {state.remedyName}</span>
                     {instructions.map((data, index) => <div key={index}>
-                        <span className="mt-2">{instructions[index]}</span><br />
+                        <span className="rmm mt-2 fs-6">{instructions[index]}</span><br />
                     </div>)}
-                    <span className="mt-2 fs-4">caution of {state.remedyName}</span>
-                    <span className="mt-2">{state.caution}</span>
-                    <button onClick={back} className="btnn text-white mt-3">Back</button>
+                    <span className="rmm mt-2 fs-4">caution of {state.remedyName}</span>
+                    <span className="rmm mt-2 fs-6">{state.caution}</span>
+                    <button onClick={back} className="btnn text-white m-2">Back</button>
                 </div>
             </div>
         </div>
-        <Footer/>
+        <Footer />
     </>
 }
